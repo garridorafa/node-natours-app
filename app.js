43a1adf8f36871f03008dbcd3c6ac7034f97f6d3
Rafael Garrido <rafaelgarrido@tecnologiateradata15.com>
2021-05-14 00:20:18 -0400

    Add get tours

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello there!', app: 'Natours' });
+const tours = JSON.parse(fs.readFileSync('./dev-data/data/tours-simple.json'));
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 const port = 3000;
