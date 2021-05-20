2538c0ba1da0221c215a0dcc9debba5054cecdda
Rafa Garrido <rafagarridolara@gmail.com>
2021-05-20 01:24:45 -0400

    Add update tour

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.get('/api/v1/tours', (req, res) => {
 
 app.get('/api/v1/tours/:id', (req, res) => {
   const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id == id);
+  const tour = tours.find((el) => el.id === id);
 
   if (!tour) {
     return res.status(404).json({
@@ -56,6 +56,25 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id == id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>',
+    },
+  });
 });
 
 const port = 3000;
