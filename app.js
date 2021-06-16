ae752caa628fcca34f127a7b1abcd80192c70be2
Rafa Garrido <rafagarridolara@gmail.com>
2021-06-16 01:26:20 -0400

    Serve public static files

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const app = express();
 
 // Middelwares
 app.use(morgan('dev'));
-
 app.use(express.json());
+app.use(express.static('./public'));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
